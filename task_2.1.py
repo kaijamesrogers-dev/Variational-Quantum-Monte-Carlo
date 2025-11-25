70326d8e830fe3a1c598d9f2a9be863522c266bf
Kai Rogers <kaijamesrogers@gmail.com>
2025-11-25 10:52:38 +0000

    2

--- task_2.1.py
+++ task_2.1.py
@@ -1,25 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Exact test wavefunction: harmonic oscillator ground state
 def w(x):
-    # wave function for n = 0
     return np.exp(-x**2/2)
 
 def w2_exact(x):
-    # analytic second derivative of wave function
+    # analytic second derivative of exp(-x^2/2)
     return (x**2 - 1) * np.exp(-x**2/2)
 
+# 3-point (2nd order) central difference
 def w2_2nd(x, h):
-    # 3-point (2nd order) central difference
     return (w(x+h) - 2*w(x) + w(x-h)) / h**2
 
+# 5-point (4th order) central difference
 def w2_4th(x, h):
-    # 5-point (4th order) central difference
-    return (16*w(x-h) + 16*w(x+h) -w(x+2*h) - w(x-2*h) - 30*w(x)) / (12*h**2)
+    return (-w(x+2*h) + 16*w(x+h) - 30*w(x) + 16*w(x-h) - w(x-2*h)) / (12*h**2)
 
 # Range of h values to test
-hs = np.linspace(0, 1, 100)[1:]  # avoid h=0
-x0 = 0.3   # choose any point you like
+hs = np.logspace(-6, -0.5, 150)
+x0 = 0.998   # choose any point you like
 
 errors_2nd = []
 errors_4th = []
@@ -29,14 +29,19 @@ for h in hs:
     errors_4th.append(abs(w2_4th(x0,h) - w2_exact(x0)))
 
 # Plot
-plt.plot(hs, errors_2nd, label='3-point (2nd order)')
-plt.plot(hs, hs**2, '--', label='h^2', color='gray')
-plt.title('Log Absolute Error in Second Derivative Approximation')
-plt.xlabel('Log(h)')
-plt.ylabel('Log Absolute Error')
-plt.grid(True)
+plt.figure(figsize=(7,5))
+plt.loglog(hs, errors_2nd, label="2nd-order (3-point)", linewidth=2)
+plt.loglog(hs, errors_4th, label="4th-order (5-point)", linewidth=2)
+
+# Plot reference slopes h^2 and h^4
+plt.loglog(hs, hs**2 * errors_2nd[10]/hs[10]**2, 'k--', label='slope = 2')
+plt.loglog(hs, hs**4 * errors_4th[10]/hs[10]**4, 'k:', label='slope = 4')
+
+plt.xlabel("h")
+plt.ylabel("Error in second derivative")
+plt.title("Finite Difference Error Scaling")
 plt.legend()
+plt.grid(True, which="both")
 plt.show()
 
-#yo
 
